3bbdc45a4f9b8e66179d5899e30521af7db08797
GustavoZaraT <sit.gizc@gmail.com>
2023-03-17 12:20:32 -0600

    IntroPy

---
+++ intro.py
@@ -0,0 +1,18 @@
+    print("ok")
+if 10>5
+    print("vredadero")
+    if 10<20:
+        print("verdadero")
+elif 5>3 # comienza segundo condicional
+    print ("esto no se imprimiria")
+else:
+    print ("aquí nunca llega")
+
+#~~~~~~~~~~~~~
+#/*Funciones*/
+#~~~~~~~~~~~~~
+def say_hello(name):
+    print("Hello", name)
+    print("Welcome to Python Tutorials")
+
+say_hello("ZaraT")
